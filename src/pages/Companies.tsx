959f8d74c96684e7e107cb5c7133e6bc780f25a2
Matthew Jimenez <mattjimenez091@gmail.com>
2022-05-25 14:04:40 -0800

    Revert "add company and detail page"
    This reverts commit 4710686c7a31d3bf32b3f76f1f77cf12c60dd3b7.

--- src/pages/Companies.tsx
+++ src/pages/Companies.tsx
@@ -1,49 +1,7 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import CompanyItem from "../components/CompanyItem";
-import { useIex } from "../context/IEXProvider";
-import "./companies.scss";
+import React from "react";
 
 const CompaniesPage = React.memo(() => {
-  const client = useIex();
-  const [companyList, setCompanyList] = useState([]);
-
-  let interval: { current: NodeJS.Timeout | null } = useRef(null);
-
-  const getCompanyList = () => {
-    client.getTopMarketCap().then((res) => {
-      if (res && res["status"] === 200 && res["data"]) {
-        setCompanyList(JSON.parse(res["data"]));
-      }
-    });
-  };
-
-  useEffect(() => {
-    getCompanyList();
-    interval.current = setInterval(() => {
-      getCompanyList();
-    }, 10000);
-    return () => clearInterval(interval.current as NodeJS.Timeout);
-  }, []);
-
-  return (
-    <Container>
-      <div className="company-list-container">
-        <Row>
-          {companyList
-            ? companyList.map((company, index) => {
-                return (
-                  <Col key={index} lg={4} md={4} sm={12}>
-                    <CompanyItem company={company} />
-                  </Col>
-                );
-              })
-            : null}
-          {companyList && !companyList.length ? <p>No Data Found</p> : null}
-        </Row>
-      </div>
-    </Container>
-  );
+  return <div>Companies Page</div>;
 });
 
 export default CompaniesPage;
